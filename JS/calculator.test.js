a70b33b1189cc16dbe0f4862a022b67c8fd22632
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2022-05-26 15:15:20 +0200

    Third Commit

--- JS/calculator.test.js
+++ JS/calculator.test.js
@@ -1,26 +1,25 @@
 describe("Calculator", function() {
     describe("add", function() {
-        it("shouldadd two numbers", function() {
+        it("should be able to add two numbers", function() {
         assert.equal(add(1, 2), 3);
         });
     });
 
     describe("subtract", function() {
-        it("shouldsubtract two numbers", function() {
+        it("should be able to subtract two numbers", function() {
         assert.equal(subtract(3, 2), 1);
         });
     });
 
     describe("multiply", function() {
-        it("shouldmultiply two numbers", function() {
+        it("should be able to multiply two numbers", function() {
         assert.equal(multiply(3, 2), 6);
         });
     });
 
     describe("divide", function() {
-        it("should ba ble divide two numbers", function() {
+        it("should be able to divide two numbers", function() {
         assert.equal(divide(6, 2), 3);
         });
     });
-
 });
